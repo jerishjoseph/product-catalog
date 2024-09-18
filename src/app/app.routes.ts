15c7aa628e97effb7ba23106b939525c92badbfd
jerishjoseph <jerishjoseph.s@gmail.com>
2024-09-18 21:10:33 +0530

    code for product catalog

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'products', component: ProductsComponent},
+    {path: 'about', component: AboutComponent},
+    {path: '', component: ProductsComponent},
+    {path: '**', component: ProductsComponent}
+];
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.scss'
+})
+export class ProductsComponent implements OnInit {
+    products = require("../../products.json");
+    items: any;
+    filterOption = '';
+
+    ngOnInit() {
+      this.items = this.products['productDetails'];
+    }
+
+    onSelect(category: any) {
+      let result: any = [];
+      this.products['productDetails']?.forEach((item: any) => {
+        if(item.category == category) {
+          result.push(item)
+        }
+      });
+      this.items = result;
+    }
+
+    sort(field: any) {
+      let result: any = this.products['productDetails'];
+      result?.sort((a: any,b: any)=>{
+        return a[field] - b[field]
+      })
+      this.items = result;
+    }
+}
